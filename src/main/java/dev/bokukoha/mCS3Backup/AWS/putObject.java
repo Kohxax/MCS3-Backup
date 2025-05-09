5245f31cb05151f39e6427e2194240fab5d649a1
Kohxax <koharen0256@gmail.com>
2025-05-09 12:38:26 +0900

    AWS依存関係追加

---
+++ src/main/java/dev/bokukoha/mCS3Backup/AWS/putObject.java
@@ -0,0 +1,16 @@
+package dev.bokukoha.mCS3Backup.AWS;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.awscore.exception.AwsErrorDetails;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+
+import java.io.UncheckedIOException;
+import java.nio.file.Path;
+
+public class putObject {
+
+}
