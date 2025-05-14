f63d22aed18fe72452a4bad833a87cd1079960e3
Kohxax <koharen0256@gmail.com>
2025-05-15 00:07:00 +0900

    バックアップ作成部分クラス作成

---
+++ src/main/java/dev/bokukoha/mCS3Backup/Backup/makeBackup.java
@@ -0,0 +1,14 @@
+package dev.bokukoha.mCS3Backup.Backup;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.*;
+import java.nio.file.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class makeBackup {
+}
