21e988d0574d675a7c401544eb4814675b40ab3e
Kohxax <koharen0256@gmail.com>
2025-05-15 00:06:46 +0900

    putObject作成

--- src/main/java/dev/bokukoha/mCS3Backup/AWS/putObject.java
+++ src/main/java/dev/bokukoha/mCS3Backup/AWS/putObject.java
@@ -1,16 +1,47 @@
 package dev.bokukoha.mCS3Backup.AWS;
 
+//AWS SDK for Java v2
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import software.amazon.awssdk.awscore.exception.AwsErrorDetails;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.StorageClass;
 
-import java.io.UncheckedIOException;
+//Javaのやつ
 import java.nio.file.Path;
 
+//S3にファイルをアップロードする
 public class putObject {
+    public static final Logger logger = LoggerFactory.getLogger(putObject.class);
 
+    public static void uploadToS3(String bucketName, String objectKey, Path filePath) {
+        // S3クライアントを作成
+        S3Client s3 = S3Client.builder()
+                .region(Region.AP_NORTHEAST_1)
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                .build();
+
+        // アップロードリクエストを作成
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(objectKey)
+                .storageClass(StorageClass.DEEP_ARCHIVE)
+                .build();
+
+        try {
+            // ファイルをアップロード
+            PutObjectResponse response = s3.putObject(putObjectRequest, RequestBody.fromFile(filePath));
+            logger.info("File uploaded successfully. ETag: " + response.eTag());
+        } catch (S3Exception e) {
+            logger.error("Failed to upload file to S3: " + e.awsErrorDetails().errorMessage());
+        } finally {
+            // S3クライアントを閉じる
+            s3.close();
+        }
+    }
 }
